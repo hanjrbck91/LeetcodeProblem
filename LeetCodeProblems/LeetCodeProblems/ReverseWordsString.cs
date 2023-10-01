d78b6072ea694852a0d4e8bf76a12f51b1c36173
Muhammed Hadhil <hadhilnjr@gmail.com>
2023-10-01 17:21:54 +0530

    updated

--- LeetCodeProblems/LeetCodeProblems/ReverseWordsString.cs
+++ LeetCodeProblems/LeetCodeProblems/ReverseWordsString.cs
@@ -7,6 +7,38 @@ using System.Threading.Tasks;
 namespace LeetCodeProblems
 {
     internal class ReverseWordsString
-    {
+    {/// <summary>
+    /// Leetcode Qestion 557
+    /// </summary>
+        public class Solution
+        {
+            public string ReverseWords(string s)
+            {
+                StringBuilder result = new StringBuilder();
+                int start = 0;
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[i] == ' ')
+                    {
+                        for (int j = i - 1; j >= start; j--)
+                        {
+                            result.Append(s[j]);
+                        }
+                        result.Append(' '); // Add the space after reversing the word
+                        start = i + 1; // Set the start index for the next word
+                    }
+                }
+
+                // Reverse the last word
+                for (int j = s.Length - 1; j >= start; j--)
+                {
+                    result.Append(s[j]);
+                }
+
+                return result.ToString();
+            }
+        }
+
     }
 }
