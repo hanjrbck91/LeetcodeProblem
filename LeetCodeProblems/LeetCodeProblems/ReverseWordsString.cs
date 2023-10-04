442df4cbf61f07baaa1098e0e4346ecfbd9780d6
Muhammed Hadhil <hadhilnjrbrototype@gmail.com>
2023-10-04 20:10:26 +0530

    5-10-2023

--- LeetCodeProblems/LeetCodeProblems/ReverseWordsString.cs
+++ LeetCodeProblems/LeetCodeProblems/ReverseWordsString.cs
@@ -152,7 +152,33 @@ namespace LeetCodeProblems
 
                 return discountPrice;
             }
-      
+        /// <summary>
+        /// Leetcode 1486
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="start"></param>
+        /// <returns></returns>
+        public int XorOperation(int n, int start)
+        {
+
+            int[] nums = new int[n];
+
+
+            for (int i = 0; i < n; i++)
+            {
+                nums[i] = start + (2 * i);
+            }
+            int result = 0;
+
+            foreach (int s in nums)
+            {
+                result ^= s;
+            }
+
+            return result;
+
+        }
+
 
 
     }
