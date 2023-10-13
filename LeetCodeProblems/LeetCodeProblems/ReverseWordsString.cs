df012f56798a93dc7ca10d0f77d0de70f564f752
Muhammed Hadhil <hadhilnjrbrototype@gmail.com>
2023-10-13 10:48:45 +0530

    13-10-2023

--- LeetCodeProblems/LeetCodeProblems/ReverseWordsString.cs
+++ LeetCodeProblems/LeetCodeProblems/ReverseWordsString.cs
@@ -332,6 +332,34 @@ namespace LeetCodeProblems
             return result.ToString();
         }
 
+        /// <summary>
+        /// Leetcode 1608
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int SpecialArray(int[] nums)
+        {
+            Array.Sort(nums);
+
+            for (int x = 1; x <= nums.Length; x++)
+            {
+                int count = 0;
+                foreach (var num in nums)
+                {
+                    if (num >= x)
+                    {
+                        count++;
+                    }
+                }
+                if (count == x)
+                {
+                    return x;
+                }
+            }
+
+            return -1;
+        }
+
 
     }
 
